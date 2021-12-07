fc427e7a757d88d32d8e021dd87f854b76e5629b
PostMetalFun1992 <kabanovkirill54@protonmail.com>
2021-12-07 22:39:55 +0300

    Added lualine plugin

--- lua/plugin_manager.lua
+++ lua/plugin_manager.lua
@@ -5,9 +5,13 @@ return require('packer').startup(function()
         config = function() require'nvim-tree'.setup {} end
     }
 
+    use {
+        'nvim-lualine/lualine.nvim',
+        requires = {'kyazdani42/nvim-web-devicons'}
+    }
+
     -- Colorschemes
     use 'nanotech/jellybeans.vim'
     use 'morhetz/gruvbox'
     use 'dracula/vim'
-
 end)
--- lua/plugin_settings.lua
+++ lua/plugin_settings.lua
@@ -2,3 +2,7 @@ local cmd = vim.cmd
 local set = vim.opt
 
 cmd 'colorscheme gruvbox'
+
+require('lualine').setup {
+    options = {theme = 'gruvbox'}
+}
