f613706fe1b47caf1346b0a88c3e2c3dd19a705a
PostMetalFun1992 <kabanovkirill54@protonmail.com>
2021-12-07 22:58:05 +0300

    Added UI tweaks

--- lua/plugin_manager.lua
+++ lua/plugin_manager.lua
@@ -1,8 +1,12 @@
 return require('packer').startup(function()
+    use 'wbthomason/packer.nvim'
+
+    -- UI tweaks
+    use 'Yggdroot/indentLine'
+
     use {
         'kyazdani42/nvim-tree.lua',
-        requires = {'kyazdani42/nvim-web-devicons'},
-        config = function() require'nvim-tree'.setup {} end
+        requires = {'kyazdani42/nvim-web-devicons'}
     }
 
     use {
@@ -10,8 +14,13 @@ return require('packer').startup(function()
         requires = {'kyazdani42/nvim-web-devicons'}
     }
 
+    use 'luochen1990/rainbow'
+
     -- Colorschemes
     use 'nanotech/jellybeans.vim'
     use 'morhetz/gruvbox'
     use 'dracula/vim'
+
+    -- Code
+    use 'jiangmiao/auto-pairs'
 end)
--- lua/plugin_settings.lua
+++ lua/plugin_settings.lua
@@ -3,6 +3,14 @@ local set = vim.opt
 
 cmd 'colorscheme gruvbox'
 
+-- lualine
 require('lualine').setup {
     options = {theme = 'gruvbox'}
 }
+
+-- nvim-tree
+vim.g.nvim_tree_indent_markers = 1
+require('nvim-tree').setup()
+
+-- rainbow
+vim.g.rainbow_active = 1
