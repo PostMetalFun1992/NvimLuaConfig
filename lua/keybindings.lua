1d2a9eb059e2079b90d29dce3d589251a9f9916f
PostMetalFun1992 <kabanovkirill54@protonmail.com>
2021-12-07 23:38:52 +0300

    Turned off arrows in visual mode

--- lua/keybindings.lua
+++ lua/keybindings.lua
@@ -5,6 +5,11 @@ map('n', '<Down>', '<nop>', {noremap = true})
 map('n', '<Left>', '<nop>', {noremap = true})
 map('n', '<Right>', '<nop>', {noremap = true})
 
+map('v', '<Up>', '<nop>', {noremap = true})
+map('v', '<Down>', '<nop>', {noremap = true})
+map('v', '<Left>', '<nop>', {noremap = true})
+map('v', '<Right>', '<nop>', {noremap = true})
+
 map('n', 'tt', ':tabnew<CR>', {noremap = true})
 map('n', 't.', ':tabnext<CR>', {noremap = true})
 map('n', 't,', ':tabprevious<CR>', {noremap = true})
