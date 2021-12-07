e52fd05f6da742020c393931bb13a14fb1462b8c
PostMetalFun1992 <kabanovkirill54@protonmail.com>
2021-12-07 22:04:19 +0300

    First

---
+++ lua/basic.lua
@@ -0,0 +1,26 @@
+local set = vim.opt
+
+set.clipboard = 'unnamed'
+
+set.number = true
+set.termguicolors = true
+
+vim.cmd [[
+    filetype indent plugin on
+    syntax enable
+]]
+
+set.expandtab = true
+set.shiftwidth = 4
+set.tabstop = 4
+set.softtabstop = 4
+
+set.splitright = true
+set.splitbelow = true
+
+set.cursorline = true
+vim.cmd [[
+    hi CursorLine term=bold cterm=bold guibg=Grey40
+]]
+
+vim.g.python3_host_prog = '/usr/bin/python3'
---
+++ lua/keybindings.lua
@@ -0,0 +1,12 @@
+local map = vim.api.nvim_set_keymap
+
+map('n', '<Up>', '<nop>', {noremap = true})
+map('n', '<Down>', '<nop>', {noremap = true})
+map('n', '<Left>', '<nop>', {noremap = true})
+map('n', '<Right>', '<nop>', {noremap = true})
+
+map('n', 'tt', ':tabnew<CR>', {noremap = true})
+map('n', 't.', ':tabnext<CR>', {noremap = true})
+map('n', 't,', ':tabprevious<CR>', {noremap = true})
+
+map('n', '<C-h>', ':set hlsearch! hlsearch?<CR>', {noremap = true})
