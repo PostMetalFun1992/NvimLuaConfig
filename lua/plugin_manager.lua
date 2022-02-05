170afd41b3425d09cf365994a75ed58f6ff102c8
PostMetalFun1992 <kabanovkirill54@protonmail.com>
2022-02-05 21:31:30 +0300

    Added lsp plugin

--- lua/plugin_manager.lua
+++ lua/plugin_manager.lua
@@ -16,6 +16,11 @@ return require('packer').startup(function()
   use { 'nvim-telescope/telescope.nvim', requires = 'nvim-lua/plenary.nvim' }
 
   -----------------------------------------------------------
+  -- LSP
+  -----------------------------------------------------------
+  use { 'neovim/nvim-lspconfig' }
+
+  -----------------------------------------------------------
   -- Code
   -----------------------------------------------------------
   use { 'nvim-treesitter/nvim-treesitter' }
